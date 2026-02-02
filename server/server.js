84b877a1afe47616a04766f2acbb525e978a3eae
haripriya122753 <army122003@gmail.com>
2026-02-02 14:14:09 +0530

    started the server

--- server/server.js
+++ server/server.js
@@ -3,3 +3,9 @@ import app from "./app.js";
 
 // DATABASE CONNECTION
 connectDb();
+
+// START SERVER
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+})
