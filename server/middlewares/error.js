c306a89293d92b3a060b105eb26957af3454fb9c
haripriya122753 <army122003@gmail.com>
2026-02-02 14:54:59 +0530

    coded the error handler

---
+++ server/middlewares/error.js
@@ -0,0 +1,31 @@
+class ErrorHandler extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+export const errorMiddleware = (err, req, res, next) => {
+    err.message = err.message || 'Internal Server Error';
+    err.statusCode = err.statusCode || 500;
+
+    if(err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    if(err.name === "JsonWebTokenError") {
+        const message = "JSON Web Token is invalid. Try Again!!!";
+        err = new ErrorHandler(message, 400);
+    }
+
+    if(err.name === "TokenExpiredError") {
+        const message = "JSON Web Token is expired. Try Again!!!";
+        err = new ErrorHandler(message, 400);
+    }
+
+    if(err.name === "CastError") {
+        const message = "Resource not found. Invalid: " + err.path;
+        err = new ErrorHandler(message, 400);
+    }
+}
