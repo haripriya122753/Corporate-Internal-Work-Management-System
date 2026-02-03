13ec185440c3d42ea7ad3c74d16156012305cf6e
haripriya122753 <army122003@gmail.com>
2026-02-03 14:54:50 +0530

    coded the name & email field validations

--- server/models/user.js
+++ server/models/user.js
@@ -2,4 +2,22 @@ import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
+import { kMaxLength } from 'buffer';
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true,
+        maxLength: [30, "Name cannot exceed 30 characters"]
+    },
+     email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email address"]
+    },
+})
 
