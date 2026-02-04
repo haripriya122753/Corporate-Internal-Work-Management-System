3b150279721ae9f1fc4aaebb91126820275329a4
haripriya122753 <army122003@gmail.com>
2026-02-04 14:57:56 +0530

    Created the model of user

--- server/models/user.js
+++ server/models/user.js
@@ -5,60 +5,70 @@ import crypto from "crypto";
 import { kMaxLength } from "buffer";
 import { type } from "os";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-    trim: true,
-    maxLength: [30, "Name cannot exceed 30 characters"],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-    lowercase: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-    ],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    select: false,
-    minLength: [8, "Password must be at least 8 characters"],
-  },
-  role: {
-    type: String,
-    enum: ["Employee", "Manager", "Admin"],
-    default: "Employee",
-  },
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxLength: [30, "Name cannot exceed 30 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+      minLength: [8, "Password must be at least 8 characters"],
+    },
+    role: {
+      type: String,
+      enum: ["Employee", "Manager", "Admin"],
+      default: "Employee",
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
 
-  department: {
-    type: String,
-    trim: true,
-    default: null,
-  },
-  experties: {
-    type: [String],
-    default: null,
-  },
-  maxSubordinates: {
-    type: Number,
-    default: 10,
-    min: [1, "Min  maxSubordinates must be at least 1"],
-  },
-  assignedEmployees: [
-    {
+    department: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    experties: {
+      type: [String],
+      default: null,
+    },
+    maxSubordinates: {
+      type: Number,
+      default: 10,
+      min: [1, "Min  maxSubordinates must be at least 1"],
+    },
+    assignedEmployees: [
+      {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
+      },
+    ],
+    manager: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
     },
-  ],
-  manager: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    default: null,
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      default: null,
+    },
   },
-});
+  {
+    timestamps: true,
+  },
+);
