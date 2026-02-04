e2c9ed1c6a3a55700206f91a58e1f812a7953429
haripriya122753 <army122003@gmail.com>
2026-02-04 13:57:32 +0530

    Aded the role, password & department

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,44 @@
-import bcrypt from 'bcrypt';
-import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
-import crypto from 'crypto';
-import { kMaxLength } from 'buffer';
-
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import { kMaxLength } from "buffer";
+import { type } from "os";
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Name is required"],
-        trim: true,
-        maxLength: [30, "Name cannot exceed 30 characters"]
-    },
-     email: {
-        type: String,
-        required: [true, "Email is required"],
-        unique: true,
-        lowercase: true,
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email address"]
-    },
-})
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+    maxLength: [30, "Name cannot exceed 30 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    select: false,
+    minLength: [8, "Password must be at least 8 characters"],
+  },
+  role: {
+    type: String,
+    enum: ["Employee", "Manager", "Admin"],
+    default: "Employee",
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 
+  department: {
+    type: String,
+    trim: true,
+    default: null,
+  },
+});
